1cb92e3262d75d1e70ecc6bf79e7f8d011d5e9c4
Vir <vir.aguilera@gmail.com>
2023-04-01 12:13:54 +0800

    Primero

---
+++ router/auth.js
@@ -0,0 +1,52 @@
+const { Router } = require('express');
+const { check } = require('express-validator');
+const router = Router();
+const { 
+    userCreate,
+    userLogin,
+    pruebaDatos
+} = require('../controller/authController');
+const validarToken = require('../middelware/validarToken');
+const { generarToken } = require('../middelware/generarToken');
+
+
+/*
+Esta ruta responde a /api
+*/
+
+router.get('/', (req, res) => {
+    res.json({
+        mensaje: 'Bienvenido a la API'
+    })
+});
+
+router.get('/registro', (req, res) => {
+    res.render('registro')
+});
+
+router.get('/index', (req, res) => {
+    res.render('index')
+});
+
+router.get('/index2', (req, res) => {
+    res.render('index2')
+});
+
+router.post('/create', [
+    check('nombre').isLength({min:5}),
+    check('email').isEmail(),
+    check('password').isLength({min:8})
+], userCreate);
+
+router.post('/login', [
+    check('email').isEmail(),
+    check('password').isLength({min:8}),
+], userLogin)
+
+router.post('/prueba', [
+    check('email').isEmail(),
+    check('password').isLength({min:8}),
+    validarToken
+], pruebaDatos)
+
+module.exports = router;
